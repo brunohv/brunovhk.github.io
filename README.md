2a6731bf0434433b2c5f6318940bc3bce473f734
Bruno Vieira <brunorick11@gmail.com>
2020-08-22 23:26:33 -0300

    adição das badges

---
+++ README.md
@@ -0,0 +1,1 @@
+"# brunovhk.github.io" 
---
+++ js/index.js
@@ -0,0 +1,34 @@
+function phpIn(obj) {
+    obj.innerHTML = "3 months"
+}
+function phpOut(obj) {
+    obj.innerHTML = "PHP"
+}
+
+function mySqlIn(obj) {
+    obj.innerHTML = "3 months"
+}
+function mySqlOut(obj) {
+    obj.innerHTML = "MySQL"
+}
+
+function bootstrapIn(obj) {
+    obj.innerHTML = "6 months"
+}
+function bootstrapOut(obj) {
+    obj.innerHTML = "Bootstrap 4"
+}
+
+function jsIn(obj) {
+    obj.innerHTML = "6 months"
+}
+function jsOut(obj) {
+    obj.innerHTML = "JavaScript"
+}
+
+function linuxIn(obj) {
+    obj.innerHTML = "1 Year"
+}
+function linuxOut(obj) {
+    obj.innerHTML = "Linux"
+}
