8c453723a081601454e6ac11121fc4e11289b0db
Bruno Vieira <brunorick11@gmail.com>
2020-08-23 21:27:59 -0300

    etc

--- js/index.js
+++ js/index.js
@@ -1,34 +1,3 @@
-function phpIn(obj) {
-    obj.innerHTML = "3 months"
-}
-function phpOut(obj) {
-    obj.innerHTML = "PHP"
-}
-
-function mySqlIn(obj) {
-    obj.innerHTML = "3 months"
-}
-function mySqlOut(obj) {
-    obj.innerHTML = "MySQL"
-}
-
-function bootstrapIn(obj) {
-    obj.innerHTML = "6 months"
-}
-function bootstrapOut(obj) {
-    obj.innerHTML = "Bootstrap 4"
-}
-
-function jsIn(obj) {
-    obj.innerHTML = "6 months"
-}
-function jsOut(obj) {
-    obj.innerHTML = "JavaScript"
-}
-
-function linuxIn(obj) {
-    obj.innerHTML = "1 Year"
-}
-function linuxOut(obj) {
-    obj.innerHTML = "Linux"
-}
+$(function () {
+    $('[data-toggle="tooltip"]').tooltip()
+})
